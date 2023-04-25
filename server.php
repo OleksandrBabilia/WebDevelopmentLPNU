590ef166301a214dd409048e17b6521a7710e64c
Alex Babilia <djesssash1708@gmail.com>
2023-04-25 14:43:30 +0300

    Fixed: Data name

--- server.php
+++ server.php
@@ -1,39 +1,40 @@
 <?php
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        //HTTP Core, Gender, Group by id, ID
         $errors = array();
 
-        $data = json_decode(file_get_contents('php://input'), true);
+        $user = json_decode(file_get_contents('php://input'), true);
 
-        if (empty($data['group'])) {
+        if (empty($user['group'])) {
             $errors['group'] = 'Group field could not be empty.';
         }
 
-        if (empty($data['firstName'])) {
+        if (empty($user['firstName'])) {
             $errors['firstName'] = 'First name field could not be empty.';
-        } elseif (!preg_match('/^[a-zA-Z]+$/', $data['firstName'])) {
+        } elseif (!preg_match('/^[a-zA-Z]+$/', $user['firstName'])) {
             $errors['firstName'] = 'First name can only contain letters.';
         }
 
-        if (empty($data['lastName'])) {
+        if (empty($user['lastName'])) {
             $errors['lastName'] = 'Last name field could not be empty';
-        } elseif (!preg_match('/^[a-zA-Z]+$/', $data['lastName'])) {
+        } elseif (!preg_match('/^[a-zA-Z]+$/', $user['lastName'])) {
             $errors['lastName'] = 'Last name can only contain letters.';
         }
 
-        if (empty($data['gender'])) {
+        if (empty($user['gender'])) {
             $errors['gender'] = 'Gender field could not be empty.';
         }
 
-        if (empty($data['birthday'])) {
+        if (empty($user['birthday'])) {
             $errors['birthday'] = 'Birthday field could not be empty.';
-        } elseif (strtotime($data['birthday']) < strtotime('1950-01-01') || strtotime($data['birthday']) > strtotime('2005-01-01')) {
+        } elseif (strtotime($user['birthday']) < strtotime('1950-01-01') || strtotime($user['birthday']) > strtotime('2005-01-01')) {
             $errors['birthday'] = 'Birthday must be from 1950 year up to 2005.';
         }
 
         if (count($errors) > 0) {
             echo json_encode(array('success' => false, 'errors' => $errors));
         } else {
-            echo json_encode(array('success' => true, 'data' => $data));
+            echo json_encode(array('success' => true, 'user' => $user));
         }
     }else {
         echo json_encode(array('success' => false, 'errors' => 'Invalid request method.'));
